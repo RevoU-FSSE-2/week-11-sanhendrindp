644115569ea356000e145e098b351155423931d6
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-09-01 18:10:36 +0700

    refactor: :art: Use async/await for user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,119 +3,103 @@ const User = require("../models/user-model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const createUser = (req, res, next) => {
-  User.find({ email: req.body.email })
-    .exec()
-    .then((user) => {
-      if (user.length >= 1) {
-        return res.status(409).json({
-          Message: "Email already exist",
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              Error: err,
-            });
-          } else {
-            const user = new User({
-              _id: new mongoose.Types.ObjectId(),
-              email: req.body.email,
-              password: hash,
-            });
-            user
-              .save()
-              .then((result) => {
-                console.log(result);
-                res.status(201).json({
-                  Message: "User created successfully",
-                  // Result: result,
-                });
-              })
-              .catch((err) => {
-                console.log(err);
-                res.status(500).json({
-                  Error: err,
-                });
-              });
-          }
-        });
-      }
+const createUser = async (req, res, next) => {
+  try {
+    const existingUser = await User.findOne({ email: req.body.email });
+
+    if (existingUser) {
+      return res.status(409).json({
+        Message: "Email already exists",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    const newUser = new User({
+      _id: new mongoose.Types.ObjectId(),
+      email: req.body.email,
+      password: hashedPassword,
     });
+
+    const result = await newUser.save();
+
+    console.log(result);
+    res.status(201).json({
+      Message: "User created successfully",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error: err.message || "An error occurred while creating the user",
+    });
+  }
 };
 
-const loginUser = (req, res, next) => {
-  const { email, password } = req.body;
+const loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
 
-  User.findOne({ email })
-    .exec()
-    .then((user) => {
-      if (!user) {
-        // No user with the provided email found
-        return res.status(401).json({
-          Message: "Email is incorrect",
-        });
-      }
+    const user = await User.findOne({ email }).exec();
 
-      // Compare the provided password with the stored hashed password
-      bcrypt.compare(password, user.password, (err, result) => {
-        if (err) {
-          // Error when password is compare
-          return res.status(401).json({
-            Message: "Login failed",
-          });
-        }
-
-        if (result) {
-          // If password correct
-          const token = jwt.sign(
-            {
-              userId: user._id,
-              email: user.email,
-            },
-            process.env.JWT_KEY,
-            {
-              // Tell how long token is valid
-              expiresIn: "1h",
-            }
-          );
-          return res.status(200).json({
-            Message: "Login successful",
-            Token: token,
-          });
-        }
-
-        // Passwords incorrect
-        res.status(401).json({
-          Message: "Password is incorrect",
-        });
+    if (!user) {
+      return res.status(401).json({
+        Message: "Email is incorrect",
       });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        Error: err,
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      const token = jwt.sign(
+        {
+          userId: user._id,
+          email: user.email,
+        },
+        process.env.JWT_KEY,
+        {
+          expiresIn: "1h",
+        }
+      );
+
+      return res.status(200).json({
+        Message: "Login successful",
+        Token: token,
       });
+    }
+
+    res.status(401).json({
+      Message: "Password is incorrect",
     });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error: err.message || "An error occurred while processing the login",
+    });
+  }
 };
 
-const deleteUser = (req, res, next) => {
-  const id = req.params.id;
+const deleteUser = async (req, res, next) => {
+  try {
+    const id = req.params.id;
 
-  User.deleteOne({ _id: id })
-    .exec()
-    .then((result) => {
-      res.status(200).json({
-        Message: "User deleted successfully",
-        Result: result,
+    const result = await User.deleteOne({ _id: id }).exec();
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({
+        Message: "User not found",
       });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        Error: err,
-      });
+    }
+
+    res.status(200).json({
+      Message: "User deleted successfully",
+      Result: result,
     });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error: err.message || "An error occurred while deleting the user",
+    });
+  }
 };
 
 module.exports = { createUser, loginUser, deleteUser };
