ebc5fd02b79184d6493b2e6bfa9e46d75e44b958
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-09-01 18:48:58 +0700

    refactor: :art: Use async/await for product controller

--- controllers/product-controller.js
+++ controllers/product-controller.js
@@ -1,151 +1,158 @@
 const mongoose = require("mongoose");
 const Product = require("../models/product-model");
 
-const getAllProduct = (req, res, next) => {
-  Product.find()
-    .select("_id name price productImage")
-    .exec()
-    .then((docs) => {
-      console.log(docs);
-      const response = {
-        Message: `Success get all products`,
-        Count: docs.length,
-        Products: docs.map((doc) => {
-          return {
-            _id: doc._id,
-            name: doc.name,
-            price: doc.price,
-            productImage: doc.productImage,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/products/" + doc._id,
-            },
-          };
-        }),
-      };
+const getAllProduct = async (req, res, next) => {
+  try {
+    const docs = await Product.find()
+      .select("_id name price productImage")
+      .exec();
 
-      res.status(200).json(response);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        Error: err,
-      });
-    });
-};
+    console.log(docs);
 
-const createProduct = (req, res, next) => {
-  console.log(req.file);
-
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price,
-    // productImage: req.file.path,
-  });
-
-  // Upload image is optional
-  if (req.file) {
-    product.productImage = req.file.path;
-  }
-
-  // Store to database
-  product
-    .save()
-    .then((result) => {
-      console.log(result);
-      res.status(201).json({
-        Message: `Product created successfully`,
-        Product: {
-          _id: result._id,
-          name: result.name,
-          price: result.price,
-          productImage: result.productImage,
+    const response = {
+      Message: `Success get all products`,
+      Count: docs.length,
+      Products: docs.map((doc) => {
+        return {
+          _id: doc._id,
+          name: doc.name,
+          price: doc.price,
+          productImage: doc.productImage,
           request: {
             type: "GET",
-            url: "http://localhost:3000/products/" + result._id,
+            url: "http://localhost:3000/products/" + doc._id,
           },
-        },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        Error: err,
-      });
+        };
+      }),
+    };
+
+    res.status(200).json(response);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error: err.message || "An error occurred while fetching products",
     });
+  }
 };
 
-const getProduct = (req, res, next) => {
-  const id = req.params.id;
+const createProduct = async (req, res, next) => {
+  try {
+    console.log(req.file);
 
-  Product.findById(id)
-    .select("_id name price productImage")
-    .exec()
-    .then((doc) => {
-      console.log(doc);
-
-      // If there is a doc, status 200, else 404 not found
-      if (doc) {
-        res.status(200).json({
-          Message: `Product for ID ${id} found`,
-          Product: doc,
-        });
-      } else {
-        res.status(404).json({
-          Message: `No product found for provided ID`,
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        Error: err,
-      });
+    const product = new Product({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      price: req.body.price,
     });
-};
 
-const updateProduct = (req, res, next) => {
-  const id = req.params.id;
-  const { name, price } = req.body;
+    // Upload image is optional
+    if (req.file) {
+      product.productImage = req.file.path;
+    }
 
-  Product.updateOne({ _id: id }, { $set: { name, price } })
-    .exec()
-    .then((result) => {
-      console.log(result);
-      res.status(200).json({
-        Message: `Product for ID ${id} updated`,
-        Product: {
+    // Store to database
+    const result = await product.save();
+
+    console.log(result);
+
+    res.status(201).json({
+      Message: `Product created successfully`,
+      Product: {
+        _id: result._id,
+        name: result.name,
+        price: result.price,
+        productImage: result.productImage,
+        request: {
           type: "GET",
-          url: "http://localhost:3000/products/" + id,
+          url: "http://localhost:3000/products/" + result._id,
         },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(200).json({
-        Error: err,
-      });
+      },
     });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error: err.message || "An error occurred while creating the product",
+    });
+  }
 };
 
-const deleteProduct = (req, res, next) => {
-  const id = req.params.id;
+const getProduct = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const doc = await Product.findById(id)
+      .select("_id name price productImage")
+      .exec();
 
-  Product.deleteOne({ _id: id })
-    .exec()
-    .then((result) => {
+    console.log(doc);
+
+    if (doc) {
       res.status(200).json({
-        Message: "Product deleted successfully",
-        Result: result,
+        Message: `Product for ID ${id} found`,
+        Product: doc,
       });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        Error: err,
+    } else {
+      res.status(404).json({
+        Message: `No product found for provided ID`,
       });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error: err.message || "An error occurred while fetching the product",
     });
+  }
+};
+
+const updateProduct = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const { name, price } = req.body;
+
+    const result = await Product.updateOne(
+      { _id: id },
+      { $set: { name, price } }
+    ).exec();
+
+    console.log(result);
+
+    res.status(200).json({
+      Message: `Product for ID ${id} updated`,
+      Product: {
+        type: "GET",
+        url: "http://localhost:3000/products/" + id,
+      },
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error:
+        err.message ||
+        `An error occurred while updating the product with ID ${id}`,
+    });
+  }
+};
+
+const deleteProduct = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const result = await Product.deleteOne({ _id: id }).exec();
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({
+        Message: "Product not found",
+      });
+    }
+
+    res.status(200).json({
+      Message: "Product deleted successfully",
+      Result: result,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error: err.message || "An error occurred while deleting the product",
+    });
+  }
 };
 
 module.exports = {
