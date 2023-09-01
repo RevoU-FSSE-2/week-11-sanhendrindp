0bb46c55b773af003b73726e9435e9f363dfb1ed
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-09-01 19:13:53 +0700

    refactor: :art: Use async/await for order-controller

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -2,123 +2,128 @@ const mongoose = require("mongoose");
 const Order = require("../models/order-model");
 const Product = require("../models/product-model");
 
-const getAllOrder = (req, res, next) => {
-  Order.find()
-    .select("_id productId quantity")
-    .exec()
-    .then((docs) => {
-      console.log(docs);
-      const response = {
-        Message: `Success get all orders`,
-        Count: docs.length,
-        Orders: docs.map((doc) => {
-          return {
-            _id: doc._id,
-            productId: doc.productId,
-            quantity: doc.quantity,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/orders/" + doc._id,
-            },
-          };
-        }),
-      };
+const getAllOrder = async (req, res, next) => {
+  try {
+    const docs = await Order.find().select("_id productId quantity").exec();
 
-      res.status(200).json(response);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        Error: err,
-      });
-    });
-};
+    console.log(docs);
 
-const createOrder = (req, res, next) => {
-  Product.findById(req.body.productId)
-    .then((product) => {
-      // If productId not exist, 404 not found (can't create order)
-      if (!product) {
-        return res.status(404).json({
-          Message: "Product not found",
-        });
-      }
-
-      const order = new Order({
-        _id: new mongoose.Types.ObjectId(),
-        productId: req.body.productId,
-        quantity: req.body.quantity,
-      });
-      return order.save();
-    })
-    .then((result) => {
-      console.log(result);
-      res.status(201).json({
-        Message: `Order created successfully`,
-        Order: {
-          _id: result._id,
-          productId: result.productId,
-          quantity: result.quantity,
+    const response = {
+      Message: `Success get all orders`,
+      Count: docs.length,
+      Orders: docs.map((doc) => {
+        return {
+          _id: doc._id,
+          productId: doc.productId,
+          quantity: doc.quantity,
           request: {
             type: "GET",
-            url: "http://localhost:3000/orders/" + result._id,
+            url: "http://localhost:3000/orders/" + doc._id,
           },
+        };
+      }),
+    };
+
+    res.status(200).json(response);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error: err.message || "An error occurred while fetching orders",
+    });
+  }
+};
+
+const createOrder = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.body.productId);
+
+    // If productId not exist, 404 not found (can't create order)
+    if (!product) {
+      return res.status(404).json({
+        Message: "Product not found",
+      });
+    }
+
+    const order = new Order({
+      _id: new mongoose.Types.ObjectId(),
+      productId: req.body.productId,
+      quantity: req.body.quantity,
+    });
+
+    const result = await order.save();
+
+    console.log(result);
+
+    res.status(201).json({
+      Message: `Order created successfully`,
+      Order: {
+        _id: result._id,
+        productId: result.productId,
+        quantity: result.quantity,
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/orders/" + result._id,
         },
-      });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        Error: err,
-      });
+      },
     });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error: err.message || "An error occurred while creating the order",
+    });
+  }
 };
 
-const getOrder = (req, res, next) => {
-  const id = req.params.id;
+const getOrder = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const doc = await Order.findById(id)
+      .select("_id productId quantity")
+      .exec();
 
-  Order.findById(id)
-    .select("_id productId quantity")
-    .exec()
-    .then((doc) => {
-      console.log(doc);
+    console.log(doc);
 
-      if (doc) {
-        res.status(200).json({
-          Message: `Order for ID ${id} found`,
-          Order: doc,
-        });
-      } else {
-        res.status(404).json({
-          Message: `No order found for provided ID`,
-        });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        Error: err,
-      });
-    });
-};
-
-const deleteOrder = (req, res, next) => {
-  const id = req.params.id;
-
-  Order.deleteOne({ _id: id })
-    .exec()
-    .then((result) => {
+    if (doc) {
       res.status(200).json({
-        Message: "Order deleted successfully",
-        Result: result,
+        Message: `Order for ID ${id} found`,
+        Order: doc,
       });
-    })
-    .catch((err) => {
-      console.log(err);
-      res.status(500).json({
-        Error: err,
+    } else {
+      res.status(404).json({
+        Message: `No order found for provided ID`,
       });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error:
+        err.message ||
+        `An error occurred while fetching the order with ID ${id}`,
     });
+  }
+};
+
+const deleteOrder = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const result = await Order.deleteOne({ _id: id }).exec();
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({
+        Message: "Order not found",
+      });
+    }
+
+    res.status(200).json({
+      Message: "Order deleted successfully",
+      Result: result,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      Error: err.message || "An error occurred while deleting the order",
+    });
+  }
 };
 
 module.exports = { getAllOrder, createOrder, getOrder, deleteOrder };
